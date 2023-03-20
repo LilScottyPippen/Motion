caa5795c5adcec366461561d275de701e0d8b25e
lilscottypippen <lilscottypippen33@gmail.com>
2023-03-20 16:18:25 +0300

    Added authorization

--- user/views.py
+++ user/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 
 def authUser(request):
+    if request.user.is_anonymous:
+        if request.method == 'POST':
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                messages.info(request, "Username or password is incorrect")
+    else:
+        return redirect('/')
     return render(request, 'user/authorization.html')
 
 def regUser(request):
