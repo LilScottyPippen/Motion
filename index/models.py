471c035cb482942ca5d8e61949ae2b5354b1555e
LilScottyPippen <lilscottypippen33@gmail.com>
2023-04-01 20:11:19 +0300

    Added share note

--- index/models.py
+++ index/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from user.models import CustomUser
 
-# Create your models here.
+
 class Note(models.Model):
     note_id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE)
     title = models.TextField()
     text = models.TextField()
     url = models.CharField(max_length=100, blank=True)
+    isPublic = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
--- index/urls.py
+++ index/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import indexPage, notePage, createOrOpenNote, createNote, deleteNote
+from .views import indexPage, notePage, createOrOpenNote, createNote, deleteNote, noteShare
 
 urlpatterns = [
     path('', indexPage, name='index'),
     path('note/', createOrOpenNote, name='note'),
     path('note/<str:url>', notePage, name='notePage'),
+    path('note/<str:url>/<int:isPublic>', noteShare, name='noteShare'),
     path('createNote/', createNote, name='createNote'),
     path('deleteNote/<str:url>', deleteNote, name='deleteNote')
 ]
